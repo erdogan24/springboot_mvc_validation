6af859769797281f252d5a453732487aed30508a
erdoganakpolat <erdoganakpolat98@gmail.com>
2025-09-06 01:59:34 +0300

    customer initbinder and min,max freePasses has added.

--- src/main/java/com/springdemo/mvc/Customer.java
+++ src/main/java/com/springdemo/mvc/Customer.java
@@ -1,5 +1,7 @@
 package com.springdemo.mvc;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -12,6 +14,17 @@ public class Customer {
     private String lastName = "";
 
 
+    @Min(value=0, message = "must be greater than or equal to zero")
+    @Max(value=10, message = "must be less than or equal to 10")
+    private int freePasses;
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
+    }
 
     public String getFirstName() {
         return firstName;
--- src/main/java/com/springdemo/mvc/CustomerController.java
+++ src/main/java/com/springdemo/mvc/CustomerController.java
@@ -2,15 +2,33 @@ package com.springdemo.mvc;
 
 
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+
+
 @Controller
 public class CustomerController {
+
+
+    // add an initbinder ... to convert trim input strings
+    // remove leading and trailing whitespace
+    // resolve issue for our validation
+
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder){
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
+
 
     @GetMapping("/")
     public String showForm(Model theModel){
@@ -24,6 +42,8 @@ public class CustomerController {
     public String processForm(
             @Valid @ModelAttribute("customer") Customer theCustomer,
             BindingResult theBildingResult){
+
+        System.out.println("Last name: | " + theCustomer.getLastName() + "|");
 
         if(theBildingResult.hasErrors()){
             return "customer-form";
